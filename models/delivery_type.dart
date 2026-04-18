706aa58d40f302e09504a5f7783c8a6448a99a3c
mi130830-bit <mi130830@gmail.com>
2026-04-18 22:11:59 +0700

    Delete models directory

--- models/delivery_type.dart
+++
@@ -1,1 +0,0 @@
-enum DeliveryType { none, delivery, pickup }
--- models/payment_record.dart
+++
@@ -1,6 +0,0 @@
-class PaymentRecord {
-  final String method;
-  final double amount;
-
-  PaymentRecord({required this.method, required this.amount});
-}
